6c72d4e04b42bef46d2ee68123c3b43948950ec1
Minh <t.hoangminh2411@gmail.com>
2022-05-08 12:32:25 +0700

    a

--- src/pages/Home/HomeMenu/HomeMenu.jsx
+++ src/pages/Home/HomeMenu/HomeMenu.jsx
@@ -4,7 +4,7 @@ import { Tabs, Radio, Space } from 'antd';
 import { layDanhSachRapAction } from '../../../redux/actions/QuanLyRapAction';
 import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-// abc
+
 
 import _, { slice } from 'lodash'
 
